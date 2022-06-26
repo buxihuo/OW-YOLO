cf7e465d0b28d5cc83a386f8f2ae25a401f3fee4
buxihuo <84908793+buxihuo@users.noreply.github.com>
2022-06-26 19:39:05 +0800

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -655,10 +655,10 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
 
         # Detections matrix nx6 (xyxy, conf, cls)
         if multi_label:
-            i, j = (x[:, 5:85] > conf_thres).nonzero(as_tuple=False).T  # lu yolo
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T  # lu yolo
             x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
         else:  # best class only
-            conf, j = x[:, 5:85].max(1, keepdim=True)  #lu yolo
+            conf, j = x[:, 5:].max(1, keepdim=True)  #lu yolo
             x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
 
         # Filter by class
